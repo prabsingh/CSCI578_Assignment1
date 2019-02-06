cb11500e7e355b326fd7186f97e8506bee128307
Prabhdeep Singh <prabhis123@gmail.com>
2019-02-05 21:10:57 -0500

    added histogram

--- Assignment1.py
+++ Assignment1.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy.stats as sp
+import matplotlib.pyplot as plt
+import math
 
 vocab_size = 50
 alpha = [.8] * vocab_size
@@ -43,18 +45,24 @@ def gen_docs_mixed():
             
 
 def create_histogram():
+    #accumulate counts
     histogram_counts = np.zeros((topic_size, vocab_size), dtype=float)
     for i in range(0, topic_size * num_docs):
         for j in range(0, vocab_size):
             histogram_counts[i//num_docs][j] += doc_array[i][j]
+
     #graph the histogram
+    for i in range(0, topic_size):
+        vocab_label = np.arange(vocab_size)
+        plt.bar(vocab_label, histogram_counts[i], align="center")
+        plt.xticks(vocab_label,vocab_label)
+        plt.ylabel("Counts")
+        plt.title("Topic " + str(i) + " counts")
+        plt.show()
             
 def calc_mu_ML(data_array):
     mu = np.zeros(vocab_size, dtype=float)
-
-    if(len(data_array) == 0):
-        return mu
-    
+  
     for doc in data_array:
         for word in range(0, vocab_size):
             mu[word] += doc[word]
@@ -66,44 +74,57 @@ def calc_mu_ML(data_array):
 
     return mu
 
+def log_likelyhood(mu, doc):
+    multinomial_coeffient = math.factorial(len(doc))
+    sum_logs = 0.0
+    for i in range(0, len(doc)):
+        multinomial_coeffient /= math.factorial(doc[i])
+        probability = mu[i]
+        if (probability == 0):
+            probability = .001
+        sum_logs += doc[i] * probability
+
+    #add log of multinomial coeffient
+    sum_logs += np.log(multinomial_coeffient)
+    return sum_logs
+    
 #P(T|D) = P(D|T)P(T)/P(D)
-#P(T|D) ~ P(D|T)P(T)
-#P(T|D) ~ P(D|T) Count(T)
+#P(T|D) ~ P(D|T)
+#P(T|D) ~ P(D|T)
 def naiveBayes(doc_array):
-    random_order = np.random.permutation(50)
-
-    train = random_order[:25]
-    test = random_order[25:]
-
-    count_T = [0] * 5
-    for i in train:
-        count_T[i//10] +=1
+    train = []
+    test = []
+    for i in range(0, topic_size):
+        random_order = np.random.permutation(10)
+        #select the first 5 docs and add to train set
+        #add remaining to test set
+        for j in range(0, 5):
+            train.append((i * 10) + random_order[j])
+        for k in range(5, 10):
+            test.append((i * 10) + random_order[k])
 
     #calculate mu_array of ML 
     mu_array = [0] * topic_size
     for i in range(0, topic_size):
-        topic_doc_array = []
-        for item in train:
-            if (item//10) == i:
-                topic_doc_array.append(doc_array[i])
+        topic_doc_array = doc_array[i * 5 : (i + 1) * 5]
         mu_array[i] = calc_mu_ML(topic_doc_array)
 
     correct_predict_count = 0
     
     for doc_num in test:
-        current_large = 0
-        current_class = 0
+        current_large = -math.inf
+        current_class = None
         # find the topic with the greatest prob of generating the data
         for i in range(0, topic_size):
-            likelyhood = sp.multinomial.pmf(doc_array[doc_num], doc_size, mu_array[i])
-            prior = count_T[i]
-            if(likelyhood * prior > current_large):
-                current_large = likelyhood * prior
+            likelyhood = log_likelyhood(mu_array[i],doc_array[doc_num])
+            if(likelyhood > current_large):
+                current_large = likelyhood
                 current_class = i
 
         #compare the guess with actual
         if current_class == doc_num//10:
             correct_predict_count += 1
+        
     return correct_predict_count/25
  
 def main():
@@ -118,10 +139,9 @@ def main():
     mu_ML = [0] * topic_size
     for i in range(0, topic_size):
         mu_ML[i] = calc_mu_ML(doc_array[i * num_docs :(i + 1) * num_docs])
-
     #part 4d
     accuracy = naiveBayes(doc_array)
-
+    print("Naive Bayes Accuracy : ", accuracy)
     #part 4e
     accuracy_array = [0] * 100  
     for i in range(0, 100):
@@ -130,13 +150,13 @@ def main():
         accuracy_array[i] = naiveBayes(doc_array)
     mean = np.mean(accuracy_array)
     sd = np.std(accuracy_array)
-    print(mean)
-    print(sd)
+    print("Mean : ", mean)
+    print("SD : ", sd)
     #part 5
     gen_topics()
     gen_docs_mixed()
     accuracy_mixed = naiveBayes(doc_array)
-
+    print("Mixed Accuracy : ", accuracy_mixed)
     accuracy_array_mixed = [0] * 100
     for i in range(0, 100):
         gen_topics()
@@ -145,7 +165,7 @@ def main():
     mean_mixed = np.mean(accuracy_array_mixed)
     sd_mixed = np.std(accuracy_array_mixed)
 
-    print(mean_mixed)
-    print(sd_mixed)
+    print("Mean : ", mean_mixed)
+    print("SD : ", sd_mixed)
 
 main()
